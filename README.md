5b42ac912a4c3a733527a227ca41fabf4b181368
Lucas Antunes <lucasba8@gmail.com>
2019-12-26 01:29:30 -0300

    Implement animation generation with many gradients
    Implement command line arguments
    Add README.md
    Add a test page

---
+++ README.md
@@ -0,0 +1,93 @@
+# Gradient Animator
+
+This small JS library creates a CSS animation that interpolates between gradients. For example:
+
+## Quick start
+```
+npm install gradient-animator
+
+npx gradient-animator --name anim --gradients 0% "linear-gradient(0deg, #ffffff 0%, #000000 100%)" 100% "linear-gradient(90deg, #000000 0%, #ffffff 100%)"
+```
+
+And in your CSS file
+```
+@import url('./anim.css') /* This is the path to the file Gradient Animator generated. */
+
+.animated-gradient
+{
+    animation: anim 1s infinite alternate;
+}
+```
+
+## _Kinda_ how it works
+
+If you give Gradient Animator these arguments 
+```
+--name anim --gradients 0% "linear-gradient(0deg, #ffffff 0%, #000000 100%)" 100% "linear-gradient(90deg, #000000 0%, #ffffff 100%)" --fps 4 --resolution 4
+```
+the output will be
+
+```
+@keyframes anim 
+{
+    0.00%
+    {
+        background-image: linear-gradient(0.00deg, #ffffff 0.00%, #bfbfbf 25.00%, #808080 50.00%, #404040 75.00%, #000000 100.00%);
+    }
+    25.00%
+    {
+        background-image: linear-gradient(22.50deg, #bfbfbf 0.00%, #9f9f9f 25.00%, #808080 50.00%, #606060 75.00%, #404040 100.00%);
+    }
+    50.00%
+    {
+        background-image: linear-gradient(45.00deg, #808080 0.00%, #808080 25.00%, #808080 50.00%, #808080 75.00%, #808080 100.00%);
+    }
+    75.00%
+    {
+        background-image: linear-gradient(67.50deg, #404040 0.00%, #606060 25.00%, #808080 50.00%, #9f9f9f 75.00%, #bfbfbf 100.00%);
+    }
+    100.00%
+    {
+        background-image: linear-gradient(90.00deg, #000000 0.00%, #404040 25.00%, #808080 50.00%, #bfbfbf 75.00%, #ffffff 100.00%);
+    }
+}
+```
+
+This works by each frame of the animation setting the gradient on the element. As you can see here this animation has few keyframes and if it's played for the duration of 1 second it's going to look very laggy. I reduced fps and resolution for easier visualization, but with an fps higher than 24 the animation will appear smooth.
+
+## It's like making a gradient of gradients
+
+You might have noticed that when specifying the gradients command-line argument there are percentages followed by a gradient
+
+```
+--gradients 0% "linear-gradient(0deg, #ffffff 0%, #000000 100%)" 100% "linear-gradient(90deg, #000000 0%, #ffffff 100%)"
+```
+
+This is used to control where _(when?)_ in the animation the gradient is. It behaves the same as the color paremeters in regular gradients, they have a position. It's like making a gradient of gradients.
+
+```
+--gradients 0% "linear-gradient(0deg, #ffffff 0%, #000000 100%)" 40% "linear-gradient(0deg, #ff0000 0%, #0000ff 100%)" 100% "linear-gradient(0deg, #000000 0%, #ffffff 100%)"
+```
+This will generate an animation that goes from white-to-black to red-to-blue in 40% of the time and then goes from red-to-blue to black-to-white in the remaining 60% of the time.
+
+## The other parameters
+
+### `--name`
+Specify a name for the animation and for it's generated CSS file.
+Required.
+
+### `--duration`
+How many seconds the animation will last. This is used to calculate the total amount of keyframes in the animation.
+Optional.
+Defaults to 1.
+
+### `--fps`
+How many frames per second should the animation be. This is used to calculate the total amount of keyframes in the animation.
+Changing this is not reccommended since a value lower than 24 will give the animation a laggy feel to it and increasing this value can generate considerably larger outputs.
+Optional.
+Defaults to 24.
+
+### `--resolution`
+This specifies the 'resolution' of the gradients. Before the Gradient Animator can generate the gradients it must first convert them to have the same amount of points in the same locations, so that it can then interpolate between the gradients. This might result in a distortion of the gradient. If the gradient distorts/has the wrong colors you should increase the resolution. Keep in mind this will generate bigger, heavier animations.
+Optional.
+Defaults to 8.
--- gradientAnimator.js
+++ gradientAnimator.js
@@ -2,112 +2,171 @@ const colorInterp = require('color-interpolate');
 const rgbHex = require('rgb-hex');
 const scaleRange = require('scale-number-range');
 const lerp = require('lerp');
-const util = require('util');
+const fs = require('fs');
 
-const gradients = [
-    {
-        angle: 30,
-        colors: [
-            {color: '#3d3393', pos: 0},
-            {color: '#2b76b9', pos: 37},
-            {color: '#2cacd1', pos: 65},
-            {color: '#35eb93', pos: 100},
-        ],
-    },
-    {
-        angle: 20, 
-        colors: [
-            {color: '#6e45e2', pos: 0},
-            {color: '#88d3ce', pos: 100},
-        ],
-    },
-]
-
-const gradientTransformResolution = 8;
-
-
-const equalizedGradients = [];
-
-for(const grad of gradients)
+if(require.main === module)
 {
-    const newGrad = {
-        angle: grad.angle,
-        colors: [
-            {color: grad.colors.find(x => x.pos == 0).color, pos: 0},
-        ]
-    };
-
-    for(let i = 0; i < gradientTransformResolution; i++)
-    {
-        const percentage = (100 / gradientTransformResolution) * (i + 1);
-
-        let closestSmallestGColor = grad.colors[0];
-        let closestSmallestDist = Math.abs(percentage - closestSmallestGColor.pos);
-        let closestBiggestGColor = grad.colors[grad.colors.length - 1];
-        let closestBiggestDist = Math.abs(percentage - closestBiggestGColor.pos);
-        for(const gColor of grad.colors)
-        {
-            const dist = Math.abs(percentage - gColor.pos);
-            if(dist < closestSmallestDist && gColor.pos <= percentage)
-            {
-                closestSmallestGColor = gColor;
-                closestSmallestDist = dist;
-            }
-            else if(dist < closestBiggestDist && gColor.pos > percentage)
-            {
-                closestBiggestGColor = gColor;
-                closestBiggestDist = dist;
-            }
-        }
+    //Command-line
+    const args = parseCommandLineArgs();
+    const anim = generateAnimation(args);
 
 
-        if(percentage == 100)
-        {
-            newGrad.colors.push({
-                color: closestBiggestGColor.color, //doesn't matter if closestBiggestGColor or closestSmallestGColor, both will be the same gColor
-                pos: 100,
-            });
-        }
-        else
-        {
-            const normalizedRelativePercentage = scaleRange(percentage, closestSmallestGColor.pos, closestBiggestGColor.pos, 0, 1);
-            const pallette = colorInterp([closestSmallestGColor.color, closestBiggestGColor.color]);
-            newGrad.colors.push({
-                color: '#' + rgbHex(pallette(normalizedRelativePercentage)),
-                pos: percentage,
-            });
-        }
-    }
-
-    equalizedGradients.push(newGrad);
+    fs.writeFileSync(process.cwd() + '/' + args.animationName + '.css', anim);
+}
+else
+{
+    //Required module
 }
 
-
-//You should aim for 24fps
-//Calculate your keyframe count like 24 * animDurationInSeconds
-const keyframeCount = 24;
-const animation = [];
-
-const grad1 = equalizedGradients[0];
-const grad2 = equalizedGradients[1];
-
-for(let i = 0; i < keyframeCount + 1; i++)
+function generateAnimation(options)
 {
-    const percentage = (100 / keyframeCount) * (i + 0);
+    let {gradients, animationName, keyframeCount, gradientResolution, duration, fps} = options;
 
+    if(keyframeCount == null)
+    {
+        keyframeCount = Math.floor(duration * fps);
+    }
+
+    gradients = gradients.map(x => 
+    {
+        return {
+            pos: parseInt(x.pos.substring(0, x.pos.length - 1)),
+            gradient: parseCssGradient(x.gradient),
+        }
+    });
+
+    //We first equalize all gradients so they have the same number
+    //of points at the same percentages.
+    //Take a loook at equalizeGradients for more information.
+    const equalizedGradients = equalizeGradients(gradients, gradientResolution);
+
+    const animation = [];
+
+    //keyframe
+    //  get closest smaller pos and closest biggest pos
+    //  get normalized position of keyframe relative to closest positions
+    //  generate interpolated gradient with this normalized position
+
+    for(let i = 0; i < keyframeCount + 1; i++)
+    {
+        const percentage = (100 / keyframeCount) * i;
+
+        const {small: leftGrad, big: rightGrad} = getNearestPositions(equalizedGradients, percentage);
+
+        //Normalize percentage between leftGrad pos and rightGrad pos
+        //E.g.: leftGrad.pos = 50, rightGrad.pos = 60, percentage = 55, normalizedPercentageBetweenGrads = .5
+        //This will allow us to get the interpolated gradient between leftGrad and rightGrad for this keyframe
+        const normalizedPercentageBetweenGrads = scaleRange(percentage, leftGrad.pos, rightGrad.pos, 0, 1);
+
+        const interpolatedGradient = lerpGradient(leftGrad.gradient, rightGrad.gradient, normalizedPercentageBetweenGrads, gradientResolution);
+
+        const keyframe = {
+            pos: percentage,
+            gradient: interpolatedGradient,
+        }
+
+        animation.push(keyframe);
+    }
+
+    return toCssAnimation(animation, animationName);
+}
+
+/**
+ * Makes all gradients have colors at the same positions based on the gradientResolution
+ * so we can interpolate between them later.
+ * Example: (I wrote the gradients like gradient(black 0%... so that it's easier to understand the example)
+ * gradients: [gradient(black 0%, white 100%), gradient(blue 0%, red 100%)]
+ * gradientResolution: 2
+ * returned value: [gradient(black 0%, 50% grey, white 100%), gradient(blue 0%, purple 50%, red 100%)]
+ * @param {*} gradients Array of gradient objects
+ * @param {*} gradientResolution 
+ */
+function equalizeGradients(gradients, gradientResolution)
+{
+    const equalizedGradients = [];
+
+    for(const gradPos of gradients)
+    {
+        const grad = gradPos.gradient;
+
+        const newGrad = {
+            angle: grad.angle,
+            colors: []
+        };
+
+        for(let i = 0; i < gradientResolution + 1; i++)
+        {
+            const percentage = (100 / gradientResolution) * i;
+
+            let closestSmallestGColor = grad.colors[0];
+            let closestSmallestDist = Math.abs(percentage - closestSmallestGColor.pos);
+            let closestBiggestGColor = grad.colors[grad.colors.length - 1];
+            let closestBiggestDist = Math.abs(percentage - closestBiggestGColor.pos);
+            for(const gColor of grad.colors)
+            {
+                const dist = Math.abs(percentage - gColor.pos);
+                if(dist < closestSmallestDist && gColor.pos <= percentage)
+                {
+                    closestSmallestGColor = gColor;
+                    closestSmallestDist = dist;
+                }
+                else if(dist < closestBiggestDist && gColor.pos > percentage)
+                {
+                    closestBiggestGColor = gColor;
+                    closestBiggestDist = dist;
+                }
+            }
+
+
+            if(percentage == 100)
+            {
+                newGrad.colors.push({
+                    color: closestBiggestGColor.color, //doesn't matter if closestBiggestGColor or closestSmallestGColor, both will be the same gColor
+                    pos: 100,
+                });
+            }
+            else
+            {
+                const normalizedRelativePercentage = scaleRange(percentage, closestSmallestGColor.pos, closestBiggestGColor.pos, 0, 1);
+                const pallette = colorInterp([closestSmallestGColor.color, closestBiggestGColor.color]);
+                newGrad.colors.push({
+                    color: '#' + rgbHex(pallette(normalizedRelativePercentage)),
+                    pos: percentage,
+                });
+            }
+        }
+
+        equalizedGradients.push({
+            pos: gradPos.pos,
+            gradient: newGrad,
+        });
+    }
+
+    return equalizedGradients;
+}
+
+/**
+ * Lerps from A to B at time T with the gradient resolution provided.
+ * @param {*} a 
+ * @param {*} b 
+ * @param {*} t 
+ * @param {*} gradientResolution 
+ */
+function lerpGradient(a, b, t, gradientResolution)
+{
     const interpolatedGradient = {
-        angle: lerp(grad1.angle, grad2.angle, percentage / 100),
+        angle: lerp(a.angle, b.angle, t),
         colors: [],
     }
 
     //gradientTransformResolution + 1 because a resolution of 8 gives 9 elements
     //(the zero element + the ones we generated in the loop)
-    for(let ci = 0; ci < gradientTransformResolution; ci++)
+    for(let ci = 0; ci < gradientResolution + 1; ci++)
     {
-        const pos = (100 / gradientTransformResolution) * (ci + 1);
+        const pos = (100 / gradientResolution) * ci;
 
-        const pallette = colorInterp([grad1.colors[ci].color, grad2.colors[ci].color]);
-        const color = '#' + rgbHex(pallette(percentage / 100));
+        const pallette = colorInterp([a.colors[ci].color, b.colors[ci].color]);
+        const color = '#' + rgbHex(pallette(t));
 
         const gColor = {
             color,
@@ -117,15 +176,46 @@ for(let i = 0; i < keyframeCount + 1; i++)
         interpolatedGradient.colors.push(gColor);
     }
 
-    const keyframe = {
-        pos: percentage,
-        gradient: interpolatedGradient,
-    }
-
-    animation.push(keyframe);
+    return interpolatedGradient;
 }
 
-console.log(toCssAnimation(animation, 'grad'));
+/**
+ * Finds the two objects with the nearest pos values
+ * around the pos parameter.
+ * Note: the elements in the objects parameter have to be sorted
+ * so that their order is from smallest pos to biggest pos
+ * Say we have an array of objects like
+ * [{pos: 0}, {pos: 25}, {pos: 50}, {pos: 75}, {pos: 100}]
+ * and the pos parameter is 48 the return value will be
+ * {small: {pos: 25}, big: {pos: 50}}
+ * @param {[{pos: Number}]} objects 
+ */
+function getNearestPositions(objects, pos)
+{
+    let smallestObj = objects[0];
+    let smallestDist = Math.abs(pos - smallestObj.pos);
+    let biggestObj = objects[objects.length - 1];
+    let biggestDist = Math.abs(pos - biggestObj.pos);
+    for(const obj of objects)
+    {
+        const dist = Math.abs(pos - obj.pos);
+        if(dist < smallestDist && obj.pos <= pos && obj.pos < biggestObj.pos)
+        {
+            smallestObj = obj;
+            smallestDist = dist;
+        }
+        else if(dist < biggestDist && obj.pos > pos)
+        {
+            biggestObj = obj;
+            biggestDist = dist;
+        }
+    }
+
+    return {
+        small: smallestObj,
+        big: biggestObj,
+    };
+}
 
 function toCssGradient(grad)
 {
@@ -154,3 +244,95 @@ function toCssAnimation(anim, name)
 
     return str;
 }
+
+function parseCssGradient(str)
+{
+    const regex = /linear-gradient\((.*)deg,(.*)\)/;
+    const results = regex.exec(str);
+
+    const gradient = {
+        angle: results[1] ? parseFloat(results[1]) : 0,
+        colors: [],
+    };
+
+    const gColors = results[2].split(',');
+    for(const gColor of gColors)
+    {
+        const components = gColor.split(' ');
+
+        gradient.colors.push({
+            color: components[1],
+            pos: parseFloat(components[2].substr(0, components[1].length - 2)),
+        });
+    }
+
+    return gradient;
+}
+
+function parseCommandLineArgs()
+{
+    const args = process.argv.slice(2, process.argv.length);
+
+    const argData = [];
+
+    for(const arg of args)
+    {
+        if(arg[0] == '-' && arg[1] == '-')
+        {
+            argData.push({arg, params: []});
+        }
+        else
+        {
+            argData[argData.length - 1].params.push(arg);
+        }
+    }
+
+    const defSingleArg = function(name)
+    {
+        const arg = argData.find(x => x.arg == name);
+        if(arg)
+        {
+            return arg.params[0];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    const retVal = {
+        animationName: defSingleArg('--name') || '',
+        gradients: [],
+        duration: parseInt(defSingleArg('--duration')) || 1,
+        fps: parseInt(defSingleArg('--fps')) || 24,
+        gradientResolution: parseInt(defSingleArg('--resolution')) || 8,
+    }
+
+    //Gradient params are position-gradient pairs
+    //E.g.:
+    //0% linear-gradient(...) 100% linear-gradient(...)
+    //Is two gradients, one at position 0 in the animation
+    //and the other at position 100 in the animation
+    const gradientsArg = argData.find(x => x.arg == '--gradients');
+    if(gradientsArg)
+    {
+        for(let i = 0; i < gradientsArg.params.length; i++)
+        {
+            const param = gradientsArg.params[i];
+            if(!(i % 2))
+            {
+                retVal.gradients.push({
+                    pos: param,
+                });
+            }
+            else
+            {
+                retVal.gradients[retVal.gradients.length - 1].gradient = param;
+            }            
+        }
+    }
+
+    return retVal;
+}
+
+module.exports.generateAnimation = generateAnimation;
